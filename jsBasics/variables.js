8eb843149134e0e432b07bf16584ec5e0bbbe4bb
Abhishek Verma <83180415+abhishekverma051@users.noreply.github.com>
2024-06-06 22:49:37 +0000

    datatypes  of javascript

---
+++ jsBasics/variables.js
@@ -0,0 +1,6 @@
+const accountId=11111
+let accountEmail = "abhiskvrm5@gmail.com"
+var accountPassword = "12345"
+accountCity = "Himachal"
+// accountIs = 2 - not allowed as it is a constantvar
+console.table([accountId,accountCity,accountPassword,accountEmail])
