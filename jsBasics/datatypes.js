1d3ad3d6ef1e917e62df4d62e98a83d2f0ee49fc
Abhishek Verma <83180415+abhishekverma051@users.noreply.github.com>
2024-06-06 22:50:10 +0000

    conversions

--- jsBasics/datatypes.js
+++ jsBasics/datatypes.js
@@ -1,2 +1,12 @@
 "use strict"; // treat all JS code as newer version
 // alert(3+3) we are using nodejs not browser so we cannot usealert
+// tc.39 for js documenttion
+// primitive datatypes
+//number 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+//null => standalone value
+// undefined => not defined yet
+// symbol => unique
+// typeof variablename defined the type of datatype typeof()
