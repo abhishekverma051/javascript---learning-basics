380f52c286837cf7a100f9690db36f0fcb55124c
Abhishek Verma <83180415+abhishekverma051@users.noreply.github.com>
2024-06-06 23:03:01 +0000

    conversion b/w diff datatypes

--- jsBasics/conversions.js
+++ jsBasics/conversions.js
@@ -7,3 +7,17 @@ let score ="33abc"
 let value = 3
 let negValue = -value
 console.log(negValue)
+//Return type of variables in JavaScript
+// 1) Primitive Datatypes
+// Number => number
+// String  => string
+// Boolean  => boolean
+// null  => object
+// undefined  =>  undefined
+// Symbol  =>  symbol
+// BigInt  =>  bigint
+
+// 2) Non-primitive Datatypes
+// Arrays  =>  object
+// Function  =>  function
+// Object  =>  object
