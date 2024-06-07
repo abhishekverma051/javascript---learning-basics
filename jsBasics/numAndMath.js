b01be270c39e054a75200e621c9cc72699cde14b
Abhishek Verma <83180415+abhishekverma051@users.noreply.github.com>
2024-06-07 11:18:42 +0000

    num and math of JS

---
+++ jsBasics/numAndMath.js
@@ -0,0 +1,28 @@
+// const score = 400
+// const balance = new Number(100)
+// console.log(balance)
+// console.log(balance.toString().length)
+// console.log(balance.toFixed(1)) // for making some precision value
+// const otherName = 23.44444
+// console.log(otherName.toPrecision(3)) // means to print a precise value upto some pointor index
+// const hundreds = 100000
+// console.log(hundreds.toLocaleString('en-IN')) //for  the representation of the numbers according to their count
+// en-IN means to print according to Indian currency standards
+// ++++++++++++++++++ Maths +++++++++++++++++++
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2)); // it will print the uppervalue if there is even point one value higher than the original number it will print 5
+// console.log(Math.floor(4.9)); // it will print 4
+// console.log(Math.min(4,2,4,5,3,34,1));
+// console.log(Math.max(3,5,6,7));
+console.log(Math.random()); /// will give random values bw 0-1
+
+
+
+
+
+
+
+
+
