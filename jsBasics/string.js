c3b2e579363126fb89fbea4ac8905eb161d6c8fd
Abhishek Verma <83180415+abhishekverma051@users.noreply.github.com>
2024-06-07 10:48:58 +0000

    string and its method in Js

---
+++ jsBasics/string.js
@@ -0,0 +1,21 @@
+// string concatenation
+"hello"+"abhi"
+const name = "abhi"
+const repoCount =50
+//console.log(name+repoCount+"verma");
+// suggested method for concatenation (``)
+//console.log(`hello my name is ${name} and my repo count is ${repoCount}`);
+// string indexing starts from  0 to n-1
+const gameName = new String('abhi')
+console.log(gameName[0]);
+console.log(gameName[3]);
+console.log(gameName.length);
+/// some of its basic string functions are
+// .length
+// .toUpperCase() for making a string uppercase
+// .charAt(indexNumber) for checking a particular alphabet at a particular index
+// .lowerCase() for making a string lowercase
+// .indexOf('') chechking the index number
+// .substring(0,4) print from 0-4 indexnumber
+// .replace()
+
