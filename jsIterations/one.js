fcf747c54b642bf63a828c79505019c273be324f
Abhishek Verma <83180415+abhishekverma051@users.noreply.github.com>
2024-06-17 13:22:45 +0000

    loops

---
+++ jsIterations/one.js
@@ -0,0 +1,14 @@
+ // for
+//  for (let i = 0; i < array.length; i++) {
+//     const element = array[i];
+    
+//  }
+ for (let i = 0; i <= 10; i++) {
+   console.log(`outer loop value: ${i}`);
+   for (let j = 0; j <=10; j++) {
+      console.log(`inner loop value ${j}and inner loop ${i}`);
+     
+ }
+}
+
+// break and continue
