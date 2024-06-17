4501ef009eb029d15b57f4f2f64824e0ab453888
Abhishek Verma <83180415+abhishekverma051@users.noreply.github.com>
2024-06-17 11:51:08 +0000

    conditional statements in js

--- jsControlFlow04/one.js
+++ jsControlFlow04/one.js
@@ -1,4 +1,8 @@
 // if
-if(condition){
-    
+const isUserLoggedIn = true
+const temperature = 50
+if(temperature < 50 ){
+    console.log("less than 50");
+}else{
+    console.log(`temperature is greater than ${temperature}`);
 }
