02d0942d81f0a45d8494e64dcc6bd478eb2b7148
Abhishek Verma <83180415+abhishekverma051@users.noreply.github.com>
2024-06-25 13:07:27 +0000

    filter in Array

---
+++ jsIterations/filter.js
@@ -0,0 +1,45 @@
+// const arr = [ 
+//     { name: "Arush", },
+//    { name: "Gaurav", }, 
+//    { name: "Abhishek", }, 
+//    { name: "Ruqayya", },
+//     ]; 
+//     let namearr = [] 
+//    arr.forEach((obj) => {
+//     namearr.push(obj.name);
+//   });
+//   console.log(namearr)
+  
+
+  
+
+//   const arr = [ 
+//     { name: "Arush", age: 25 },
+//     { name: "Gaurav", age: 20 }, 
+//     { name: "Abhishek", age: 30 }, 
+//     { name: "Ruqayya", age: 18 },
+//   ]; 
+//   let nme = []
+//   arr.forEach((obj)=>nme.push(obj.name,obj.age) )
+  
+  
+//   const filteredArr = arr.filter((obj) => obj.age > 21);
+//   console.log(filteredArr);
+
+  
+
+  const arr = [ 
+    { name: "Arush", age: 26, isActive: true },
+    { name: "Gaurav", age: 22, isActive: false }, 
+    { name: "Abhishek", age: 21, isActive: true },
+    { name: "Ruqayya", age: 19, isActive: false }
+  ]; 
+  let nme = []
+  arr.forEach((obj)=>nme.push(obj.name,obj.age,obj.isActive) )
+  const filteredArr = arr.filter((obj) => obj.isActive === true);
+  
+  console.log(filteredArr);
+  
+
+
+
