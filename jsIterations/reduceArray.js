8b840822466fe701a2978c013a6b0cd7f991387a
Abhishek Verma <83180415+abhishekverma051@users.noreply.github.com>
2024-06-26 10:12:58 +0000

    array methods

---
+++ jsIterations/reduceArray.js
@@ -0,0 +1,36 @@
+const myNums = [1, 2, 3]
+
+// const myTotal = myNums.reduce(function (acc, currval) {
+//     console.log(`acc: ${acc} and currval: ${currval}`);
+//     return acc + currval
+// }, 0)
+// accomulator is a empty value given to us , 
+// when the loop is runing for the very first tine then the value of the inital value is gone into the accumulator
+//then the result of the operation is stored into the accumulator
+const myTotal = myNums.reduce( (acc, curr) => acc+curr, 0)
+
+console.log(myTotal);
+
+
+const shoppingCart = [
+    {
+        itemName: "js course",
+        price: 2999
+    },
+    {
+        itemName: "py course",
+        price: 999
+    },
+    {
+        itemName: "mobile dev course",
+        price: 5999
+    },
+    {
+        itemName: "data science course",
+        price: 12999
+    },
+]
+
+const priceToPay = shoppingCart.reduce((acc, item) => acc + item.price, 0)
+
+console.log(priceToPay);
